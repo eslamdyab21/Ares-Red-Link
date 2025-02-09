62badab33868b8c2598d8791347609156475c02d
eslamdyab21 <eslamdyab22@gmail.com>
2025-02-09 08:23:33 +0200

    ephemeris for mars and sun for current day only

--- ephemeris/ephemeris_data.cpp
+++ ephemeris/ephemeris_data.cpp
@@ -114,3 +114,116 @@ const std::vector<EphemerisEntry>& EphemerisData::getSunEphemerisData() const {
 const std::vector<EphemerisEntry>& EphemerisData::getMarsEphemerisData() const {
     return mars_ephemeris_data;
 }
+
+
+
+EphemerisEntry EphemerisData::getSunEphemerisDataDate(const std::string& astro_object_filename, std::string date) {
+    std::ifstream astro_object_file(astro_object_filename);
+    std::string line;
+    
+    if (!astro_object_file.is_open()) {
+        std::cerr << "Error: Unable to open ephemeris files." << std::endl;
+        return EphemerisEntry();
+    }
+    
+    // Skip header
+    std::getline(astro_object_file, line);
+
+    double ra_h, ra_m, ra_s;
+    double dec_d, dec_m, dec_s;
+    while (std::getline(astro_object_file, line)) {
+        std::istringstream iss(line);
+        EphemerisEntry entry;
+        std::string temp;
+
+        // Read required columns
+        std::getline(iss, entry.date, ',');
+
+        // Skip JD (not needed)
+        std::getline(iss, temp, ',');
+        // Skip App GST (not needed)
+        std::getline(iss, temp, ',');
+        // Skip Equation of Time (not needed)
+        std::getline(iss, temp, ',');
+
+
+        // Read RA (h, m, s)
+        std::getline(iss, temp, ' '); ra_h = std::stod(temp);
+        std::getline(iss, temp, ' '); ra_m = std::stod(temp);
+        std::getline(iss, temp, ','); ra_s = std::stod(temp);
+        // Convert RA to decimal hours
+        std::getline(iss, temp, ','); entry.ra = ra_h + (ra_m / 60.0) + (ra_s / 3600.0);
+
+
+        // Read Apparent Declination (±DD:MM.M)
+        std::getline(iss, temp, ' '); dec_d = std::stoi(temp);
+        std::getline(iss, temp, ' '); dec_m = std::stoi(temp);
+        std::getline(iss, temp, ','); dec_s = std::stod(temp);
+        // Convert Declination to decimal degrees
+        entry.declination = dec_d + (dec_m / 60.0) + (dec_s / 3600.0);
+
+
+        // Read Declination and Distance (AU)
+        std::getline(iss, temp, ','); entry.distance_au = std::stod(temp);
+
+
+        
+        if (entry.date == date) {
+            return entry;
+        }
+    }
+
+    return EphemerisEntry();
+}
+
+
+EphemerisEntry EphemerisData::getMarsEphemerisDataDate(const std::string& astro_object_filename, std::string date) {
+    std::ifstream astro_object_file(astro_object_filename);
+    std::string line;
+    
+    if (!astro_object_file.is_open()) {
+        std::cerr << "Error: Unable to open ephemeris files." << std::endl;
+        return EphemerisEntry();
+    }
+    
+    // Skip header
+    std::getline(astro_object_file, line);
+
+    double ra_h, ra_m, ra_s;
+    double dec_d, dec_m, dec_s;
+    while (std::getline(astro_object_file, line)) {
+        std::istringstream iss(line);
+        EphemerisEntry entry;
+        std::string temp;
+
+        // Read required columns
+        std::getline(iss, entry.date, ',');
+
+
+        // Read RA (h, m, s)
+        std::getline(iss, temp, ' '); ra_h = std::stod(temp);
+        std::getline(iss, temp, ' '); ra_m = std::stod(temp);
+        std::getline(iss, temp, ','); ra_s = std::stod(temp);
+        // Convert RA to decimal hours
+        std::getline(iss, temp, ','); entry.ra = ra_h + (ra_m / 60.0) + (ra_s / 3600.0);
+
+
+        // Read Apparent Declination (±DD:MM.M)
+        std::getline(iss, temp, ' '); dec_d = std::stoi(temp);
+        std::getline(iss, temp, ' '); dec_m = std::stoi(temp);
+        std::getline(iss, temp, ','); dec_s = std::stod(temp);
+        // Convert Declination to decimal degrees
+        entry.declination = dec_d + (dec_m / 60.0) + (dec_s / 3600.0);
+
+
+        // Read Declination and Distance (AU)
+        std::getline(iss, temp, ','); entry.distance_au = std::stod(temp);
+
+
+        if (entry.date == date) {
+            return entry;
+        }
+    }
+
+    return EphemerisEntry();
+}
