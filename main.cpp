e61e2352d3c0978b933a4e76803cf986a517e633
eslamdyab21 <eslamdyab22@gmail.com>
2025-02-12 03:50:25 +0200

    compute signal delay & coordinates for each record for each year

--- main.cpp
+++ main.cpp
@@ -1,36 +1,52 @@
 #include <iostream>
 #include <vector>
+#include <array>
 #include "ephemeris/ephemeris_data.h"
 #include "network/comms_manager.h"
+
+
 
 int main() {
     EphemerisData EphemerisData;
     CommsManager CommsManager;
 
-    std::string mars_ephemeris_file = "source-data/mars2020.csv";
-    std::string sun_ephemeris_file  = "source-data/sun2020.csv";
+    std::string mars_ephemeris_file, sun_ephemeris_file;
+    std::string years[] = {"2020", "2021"};
 
-
-    EphemerisData.loadMarsEphemeris(mars_ephemeris_file);
-    const std::vector<EphemerisEntry>& mars_ephemeris_data = EphemerisData.getMarsEphemerisData();
-
-    EphemerisData.loadSunEphemeris(sun_ephemeris_file);
-    const std::vector<EphemerisEntry>& sun_ephemeris_data = EphemerisData.getSunEphemerisData();
-
-
-
-    int size = mars_ephemeris_data.size();
+    std::array<double, 3> mars_coordinates, sun_coordinates;
     double signal_delay;
-    for (int i = 0; i < size; i++)
-    {
-        signal_delay = CommsManager.computeSignalDelay(mars_ephemeris_data[i], sun_ephemeris_data[i]);
 
-        std::cout << "Date: " << mars_ephemeris_data[i].date
-                  << ", RA: " << mars_ephemeris_data[i].ra
-                  << ", Dec: " << mars_ephemeris_data[i].declination
-                  << ", Distance (AU): " << mars_ephemeris_data[i].distance_au    
-                  << ", Delay: " << signal_delay << std::endl;   
+
+    // Compute signal delay & coordinates for each record for each year
+    for (int i = 0; i < sizeof(years) / sizeof(years[0]); i++) {
+        mars_ephemeris_file = std::string("source-data/mars") + years[i] + ".csv";
+        sun_ephemeris_file  = std::string("source-data/sun") + years[i] + ".csv";
+
+        EphemerisData.loadMarsEphemeris(mars_ephemeris_file);
+        std::vector<EphemerisEntry>& mars_ephemeris_data = EphemerisData.getMarsEphemerisData();
+
+        EphemerisData.loadSunEphemeris(sun_ephemeris_file);
+        std::vector<EphemerisEntry>& sun_ephemeris_data = EphemerisData.getSunEphemerisData();
+
+
+        for (int j = 0; j < mars_ephemeris_data.size(); j++) {
+            mars_coordinates = CommsManager.toCartesian(mars_ephemeris_data[j].ra, mars_ephemeris_data[j].declination);
+            sun_coordinates  = CommsManager.toCartesian(sun_ephemeris_data[j].ra, sun_ephemeris_data[j].declination);
+            signal_delay = CommsManager.computeSignalDelay(mars_ephemeris_data[j], sun_ephemeris_data[j]);
+
+            std::cout << "Date: " << mars_ephemeris_data[j].date << ", " << years[i]
+                    << "; RA: " << mars_ephemeris_data[j].ra
+                    << "; Dec: " << mars_ephemeris_data[j].declination
+                    << "; Mars Coordinates: " << mars_coordinates[0] << "," << mars_coordinates[1] << "," << mars_coordinates[2]
+                    << "; Sun Coordinates: " << sun_coordinates[0] << "," << sun_coordinates[1] << "," << sun_coordinates[2]
+                    << "; Distance (AU): " << mars_ephemeris_data[j].distance_au    
+                    << "; Signal Delay: " << signal_delay << std::endl;   
+        }
+
+        EphemerisData.clearEphemerisData();
     }
+
+
 
     return 0;
 }
