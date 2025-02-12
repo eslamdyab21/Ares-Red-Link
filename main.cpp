32b04a4790c3b5305ad53dae2c460802304cda59
eslamdyab21 <eslamdyab22@gmail.com>
2025-02-12 07:08:09 +0200

    log Mars-Sun angle

--- main.cpp
+++ main.cpp
@@ -17,15 +17,16 @@ int main() {
     std::ofstream outputFile("link_data.csv");
     if (outputFile.is_open()) {
         outputFile  << "Date" << ";" << "RA" << ";" << "Dec" << ";" <<"Mars Coordinates"
-                    << ";" << "Sun Coordinates" << ";" << "Distance" << ";" << "Signal Delay" << std::endl;                       
+                    << ";" << "Sun Coordinates" << ";" << "Distance" 
+                    << ";" << "Angle" << ";" << "Signal Delay" << std::endl;                       
     }
     
 
     std::string mars_ephemeris_file, sun_ephemeris_file;
-    std::string years[] = {"2020", "2021"};
+    std::string years[] = {"2020", "2021", "2022", "2023", "2024"};
 
     std::array<double, 3> mars_coordinates, sun_coordinates;
-    double signal_delay;
+    double signal_delay, mars_sun_angle;
 
 
     // Compute signal delay & coordinates for each record for each year
@@ -43,6 +44,8 @@ int main() {
         for (int j = 0; j < mars_ephemeris_data.size(); j++) {
             mars_coordinates = CommsManager.toCartesian(mars_ephemeris_data[j].ra, mars_ephemeris_data[j].declination);
             sun_coordinates  = CommsManager.toCartesian(sun_ephemeris_data[j].ra, sun_ephemeris_data[j].declination);
+            
+            mars_sun_angle = CommsManager.computeMarsSunAngle(mars_ephemeris_data[j], sun_ephemeris_data[j]);
             signal_delay = CommsManager.computeSignalDelay(mars_ephemeris_data[j], sun_ephemeris_data[j]);
 
             
@@ -53,6 +56,7 @@ int main() {
                             << ";" << mars_coordinates[0] << "," << mars_coordinates[1] << "," << mars_coordinates[2]
                             << ";" << sun_coordinates[0] << "," << sun_coordinates[1] << "," << sun_coordinates[2]
                             << ";" << mars_ephemeris_data[j].distance_au
+                            << ";" << mars_sun_angle
                             << ";" << signal_delay << std::endl;
             }
 
@@ -62,7 +66,8 @@ int main() {
                     << "; Dec: " << mars_ephemeris_data[j].declination
                     << "; Mars Coordinates: " << mars_coordinates[0] << "," << mars_coordinates[1] << "," << mars_coordinates[2]
                     << "; Sun Coordinates: " << sun_coordinates[0] << "," << sun_coordinates[1] << "," << sun_coordinates[2]
-                    << "; Distance (AU): " << mars_ephemeris_data[j].distance_au    
+                    << "; Distance (AU): " << mars_ephemeris_data[j].distance_au   
+                    << "; Angle: " << mars_sun_angle  
                     << "; Signal Delay: " << signal_delay << std::endl;   
         }
 
